b0bbe2c7d91628e8142bf436408e66fae5c5020b
Ambuh mwangi <ambuhmwangi.code@gmail.com>
2021-01-22 14:19:54 +0300

    @devambrose 1.22.2021 1419hrs update for the hire me page?

--- src/pages/hire.js
+++ src/pages/hire.js
@@ -4,7 +4,22 @@ import LayoutClass from "../components/containers/layout/layout";
 export default ()=>{
     return (
         <LayoutClass className='app-left app-full' title={'Hire Me?'}>
-            About the page
+            {
+                [
+                    {tag:'POWER OF SIMPLICITY',font:'fas fa-cloud-meatball',desc:'Start simple. Keep minimal. Deliver incredible!'},
+                    {tag:'FRONTEND CONTROLS',font:'fas fa-tools',desc:'Publish content and tweak your design without leaving the homepage'},
+                    {tag:'MOBILE VERSION',font:'fas fa-mobile-alt',desc:'Gain your mobile audience with a better dedicated design'},
+                    {tag:'EASY TO LAUNCH',font:'fas fa-rocket',desc:'lightweight applications ,fast to load even in low network areas.'},
+                    {tag:'LOVE & COFFE',font:'fas fa-coffee',desc:'built with so much commitment,love and coffe.Deliverd on time'}
+            ].map((code,key)=>(
+                <div class='app-left services'>
+                    <i className={code.font}></i>
+                    <h5>{code.tag}</h5>
+                <p>{code.desc}  </p>
+            </div>
+                ) )
+            }
+            
         </LayoutClass>
     )
 }
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -5,13 +5,14 @@ export default ()=>(
         <LayoutClass className='app-left app-full' title={"My Skills"}>
             {
                 [
-                    {name:'Main Stacks',arr:["PHP","NODE JS","PYTHON","C++",'KOTLIN |ANDROID']},
+                    {name:'Main Stacks',arr:["PHP","NODE JS","PYTHON","C++",'KOTLIN |ANDROID','ELECTRON JS']},
                     {name:'WEB CSR',arr:["REACT JS","JQUERY JS","GATSBY JS"]},
                     {name:'WEB SSR',arr:["PHP","NODE JS"]},
                     {name:'ANDROID',arr:["KOTLIN","JAVA"]},
-                    {name:'DATABASES',arr:['POSTGRESQL',"MONGO","SQLITE","MARIA DB","SQL"]},
+                    {name:'DATABASES',arr:['POSTGRESQL',"MONGO","SQLITE","MARIA DB","SQL",'FIREBASE']},
                     {name:'SOURCE CONTROL',arr:['GIT','DOCKER','KUBERNETES']},
-                    {name:'CURRENTLY PURSUING',arr:['HADOOP DATABASES','EXPRESS JS','REACT NATIVE']}
+                    {name:'CURRENTLY PURSUING',arr:['HADOOP DATABASES','EXPRESS JS','REACT NATIVE']},
+                    {name:'OPERATING SYSTEMS',arr:['Unix Based Systems',"Microsoft"]}
                 ].map( ({name,arr,codes})=>(
                     <fieldset className={'code-base'} style={{borderRadius:'5px',margin:'4% 1%',width:'92%',display:'flex'}}>
                         <legend>{name}</legend>
